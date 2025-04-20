f9fc17633b3b1849f3905551184a22e6e482e13f
Arto Kaitosaari <arto.kaitosaari@aiforia.com>
2025-04-20 23:43:10 +0300

    fix(CliTool): add missing whitespace to PartB template title

--- CliTool/Template/PartB.cs
+++ CliTool/Template/PartB.cs
@@ -2,7 +2,7 @@ using Artokai.AOC.Core;
 
 namespace Artokai.AOC.Puzzles.Y%%YYYY%%.D%%DD%%;
 
-[PuzzleInfo(year: %%YYYY%%, day: %%D%%, part: 2, title:"%%TITLE%%")]
+[PuzzleInfo(year: %%YYYY%%, day: %%D%%, part: 2, title: "%%TITLE%%")]
 public class PartB : SolverBase
 {
     public override string Solve()
