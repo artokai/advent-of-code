8923a80d988d4610ad29f3ac0acbf3586ef1529f
Arto Kaitosaari <arto.kaitosaari@aiforia.com>
2025-08-11 08:59:41 +0300

    fix: puzzle info 2016/16 part 1

--- Puzzles/Y2016/D16/PartA.cs
+++ Puzzles/Y2016/D16/PartA.cs
@@ -2,6 +2,7 @@ using Artokai.AOC.Core;
 
 namespace Artokai.AOC.Puzzles.Y2016.D16;
 
+[PuzzleInfo(year: 2016, day: 16, part: 1, title: "Dragon Checksum")]
 public class PartA : SolverBase
 {
     public override string Solve()
